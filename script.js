163f94a18965fff8d0d4a7b3f6e89dc6350ba9dc
Hexbugman213 <minecrafthex213@gmail.com>
2018-10-13 15:36:37 -0500

    more

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var grid = new Muuri('.grid', {
     dragEnabled: true,
     dragStartPredicate: function (item, event) {
         return edit;
+
     },
 });
 document.getElementById("demo").innerHTML = edit;
@@ -13,24 +14,26 @@ function toggleEdit() {
     if (edit) {
         edit = false;
         document.getElementById("demo").innerHTML = edit;
-        $( ".item-content" ).each(function( index ) {
+        $(".btn").html("Edit");
+        $( ".shake-item" ).each(function( index ) {
             $(this).css({"animation": "none !important", "animation-iteration-count": "0"})
-        });
-        $( ".close" ).each(function( index ) {
-            $(this).css({"visibility": "hidden"})
         });
     } else {
         edit = true;
         document.getElementById("demo").innerHTML = edit;
-        document.getElementById("demo").innerHTML = "shake " + rnd(1,3) + "s";
-        $( ".item-content" ).each(function( index ) {
+        $( ".shake-item" ).each(function( index ) {
             $(this).css({"animation": "shake " + rnd(1,3) + "s", "animation-iteration-count": "infinite"})
         });
-        $( ".close" ).each(function( index ) {
-            $(this).css({"visibility": "visible"})
-        });
+        $(".btn").html("Save Changes");
     }
 }
 function closeElem(elem) {
     grid.remove([$('elem1')], true);
 }
+/*
+        CODE FOR CLOSE BUTTON AND ADD - CURRENTLY UNUSED
+        $( ".close" ).each(function( index ) {
+            $(this).css({"visibility": "visible"})
+        });
+        $(".add-item").css({"visibility": "visible"})
+ */
